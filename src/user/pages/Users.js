f030b671f473ea5b3e9cbf2697feee62e93818ed
ishan51551 <ishaansahu515@gmail.com>
2022-12-17 22:17:58 +0530

    frontend

---
+++ src/user/pages/Users.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import UserList from "../components/UsersList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+
+const Users = () => {
+
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
+    const [loadedUsers, setLoadedUsers] = useState();
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const responseData = await sendRequest(
+                    'http://localhost:5001/api/users'
+                );
+
+                setLoadedUsers(responseData.users);
+           
+            }catch (err) {}
+        };
+        fetchUsers();
+    }, [sendRequest]);
+
+
+    return (
+        <React.Fragment>
+            <ErrorModal error={error} onClear={clearError} />
+            {isLoading && (
+                <div className="center">
+                    <LoadingSpinner />
+                </div>
+            )}
+            {!isLoading && loadedUsers && <UserList items={loadedUsers} />}
+        </React.Fragment>
+
+    );
+};
+
+export default Users;
